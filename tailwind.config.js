1b78340985ce94f2df65d0b33b8fa7ebe610a5d0
BijakLaut <hudaaputra72@gmail.com>
2022-08-18 22:45:48 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0284C7",
+        primedark: "#0C4A6E",
+        primelight: "#38BDF8",
+        caption: "#64748B",
+      },
+    },
+  },
+  plugins: [],
+};
